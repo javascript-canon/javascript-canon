1119c2a42b7c8db8501a634de24dee7d2e71b512
Kai Gittens <kai.gittens@gmail.com>
2015-12-01 18:27:48 -0500

    start building single page view code

--- vanilla-js-mvc/js-build/firstPageView.js
+++ vanilla-js-mvc/js-build/firstPageView.js
@@ -5,4 +5,19 @@
 // use strict mode
 "use strict";
 
-var Resources = require( "./model" );
+var
+    // "require" the model data
+    Resources = require( "./model" ),
+
+    // create a short-hand reference to the data model
+    resourcesData = Resources.ResourceModel,
+    SingleResourceView = {};
+
+  SingleResourceView.render = function() {
+    return resourcesData.on("value", function(snapshot) {
+      var data = snapshot.val();
+    });
+  }
+    
+
+SingleResourceView.render();
