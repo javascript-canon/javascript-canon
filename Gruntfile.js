18ede5b493c3b311d4bbeb3085c9fdc32236f6f9
Kai Gittens <kai.gittens@gmail.com>
2015-12-01 18:27:04 -0500

    create default grunt task, add comments & whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,23 @@
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
 
   // Authorization key for saving "resources.json" to Firebase
-  var authConfig = grunt.file.readJSON("./config/auth.json");
+  var authConfig = grunt.file.readJSON( "./config/auth.json" );
 
   // Project config
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON( "package.json" ),
 
+    // Authentication token config
     authConfig: authConfig,
+
+    // firebase task config
     firebase: {
       options: {
 
-        // reference to start with (full firebase url)
+        // reference to start with (full Firebase URL)
         reference: "https://javascriptcanon.firebaseio.com/",
 
-        // token: the secret key used for connecting to firebase 
+        // token: the secret key used for connecting to Firebase 
         token: "<%= authConfig.token %>"
       },
       load: {
@@ -29,6 +32,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks( "grunt-firebase" );
 
-  grunt.registerTask("default", ["firebase"]);
+  // Type "grunt" in the CLI and the Firebase task runs
+  grunt.registerTask( "default", ["firebase"] );
 
 };
