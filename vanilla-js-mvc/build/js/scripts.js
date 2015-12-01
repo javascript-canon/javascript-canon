31291def824ba868bcb5f3638297904ebfb6aeb7
Kai Gittens <kai.gittens@gmail.com>
2015-12-01 18:28:03 -0500

    update "build/js/scripts.js"

--- vanilla-js-mvc/build/js/scripts.js
+++ vanilla-js-mvc/build/js/scripts.js
@@ -6,7 +6,22 @@
 // use strict mode
 "use strict";
 
-var Resources = require( "./model" );
+var
+    // "require" the model data
+    Resources = require( "./model" ),
+
+    // create a short-hand reference to the data model
+    resourcesData = Resources.ResourceModel,
+    SingleResourceView = {};
+
+  SingleResourceView.render = function() {
+    return resourcesData.on("value", function(snapshot) {
+      var data = snapshot.val();
+    });
+  }
+    
+
+SingleResourceView.render();
 },{"./model":2}],2:[function(require,module,exports){
 /* ================================================================= */
 /* | MODEL DATA                                                      */
