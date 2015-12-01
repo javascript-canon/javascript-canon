055863c9853dba8173b256b49ccbaec8df39cfd9
Kai Gittens <kai.gittens@gmail.com>
2015-12-01 06:04:25 -0500

    set up firebase via Grunt (see message) -build Gruntfile.js for firebase build-outs. -add firebase auth.json token for remote authentication

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  // Authorization key for saving "resources.json" to Firebase
+  var authConfig = grunt.file.readJSON('./config/auth.json');
+
+
+  // Project config
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    authConfig: authConfig,
+    firebase: {
+      options: {
+
+        // reference to start with (full firebase url)
+        reference: 'https://javascriptcanon.firebaseio.com/',
+
+        // token: the secret key used for connecting to firebase from 
+        token: '<%= authConfig.token %>'
+      },
+      load: {
+        files: [
+          {
+            src: './resources.json'
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-firebase');
+
+};
