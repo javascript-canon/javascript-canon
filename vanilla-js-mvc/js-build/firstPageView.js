41ccbdd9a9562ec081783ed37b98119d766552f4
Kai Gittens <kai.gittens@gmail.com>
2015-12-01 19:12:12 -0500

    update SingleResourceView.render() method, require jquery, update comments

--- vanilla-js-mvc/js-build/firstPageView.js
+++ vanilla-js-mvc/js-build/firstPageView.js
@@ -9,15 +9,20 @@ var
     // "require" the model data
     Resources = require( "./model" ),
 
+    // "require" jQuery core
+    $ = require( "jquery" ),
+
     // create a short-hand reference to the data model
     resourcesData = Resources.ResourceModel,
+
+    // Create a view object for the homepage view
     SingleResourceView = {};
 
-  SingleResourceView.render = function() {
-    return resourcesData.on("value", function(snapshot) {
-      var data = snapshot.val();
-    });
-  }
-    
 
-SingleResourceView.render();
+// "render()" method" renders info for single resource component
+SingleResourceView.render = function() {
+  return resourcesData.on( "value", function( snapshot ) {
+    var data = snapshot.val();
+    console.log( data );
+  });
+}
