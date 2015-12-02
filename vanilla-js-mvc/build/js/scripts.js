49f55b4955bfedfa479c18b897a21dd82ad159b7
Kai Gittens <kai.gittens@gmail.com>
2015-12-01 19:48:36 -0500

    update vanilla-js-mvc/build/js/scripts.js

--- vanilla-js-mvc/build/js/scripts.js
+++ vanilla-js-mvc/build/js/scripts.js
@@ -3,6 +3,8 @@
 /* | VIEW FOR THE FRONT PAGE                                         */
 /* ================================================================= */
 
+// A very simple MVC implementation-read more at: http://bit.ly/1zxWh0m
+
 // use strict mode
 "use strict";
 
@@ -20,8 +22,12 @@ var
     SingleResourceView = {};
 
 
-// "render()" method" renders info for single resource component
-SingleResourceView.render = function() {
+/* 
+ * "render()" method" renders info for single resource component.
+ * The "model" parameter will represent whatever variable is storing
+ * the model data
+ */
+SingleResourceView.render = function( model ) {
   return resourcesData.on( "value", function( snapshot ) {
     var data = snapshot.val();
     console.log( data );
