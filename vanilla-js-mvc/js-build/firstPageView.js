b9d38d48207cbff0cd333033b6e02a49f09ad378
Kai Gittens <kai.gittens@gmail.com>
2015-12-03 05:39:05 -0500

    arrange the link, add comments

--- vanilla-js-mvc/js-build/firstPageView.js
+++ vanilla-js-mvc/js-build/firstPageView.js
@@ -31,14 +31,12 @@ SingleResourceView.render = function( model ) {
 
     var
         data, // will be a reference to the resource data on Firebase
-        title, // will be a reference to all the resource data title
-        image, // will be a reference to all the resource data image
-        author, // will be a reference to all the resource data author
         componentArray = [];
-        
+
     /*
-     * Grab the Firebase data with a snapshot. Read more about this
-     * at: http://bit.ly/firebase-snapshot
+     * Grab the Firebase data with a snapshot and store it in the data
+     * variable. Read more about this at:
+     * http://bit.ly/firebase-snapshot
      */
     data = snapshot.val();
 
@@ -49,12 +47,14 @@ SingleResourceView.render = function( model ) {
           resourceContainer = document.createElement( "article" ),
           resourceTitle = document.createElement( "h2" ),
           resourceImage = document.createElement( "img" ),
-          resourceAuthor = document.createElement( "h3" );
+          resourceAuthor = document.createElement( "h3" ),
+          resourceLink = document.createElement( "a" );
 
       /*
        * Setting attributes one-by-one instead of using something like 
        * $.attr(). You can say that this code isn't cool, but it's 
        * certainly faster.
+       *
        * Check this at: http://bit.ly/set-attribute-test
        */
      
@@ -67,14 +67,21 @@ SingleResourceView.render = function( model ) {
 
       // Set attributes for the resource image
       resourceImage.setAttribute( "src", "/img/book-images/" + data[key].image_large );
+      resourceImage.setAttribute( "class", "gl-book-image" );
 
       // Add the resource author to the inside of the <h3>
       resourceAuthor.innerHTML =  "by " + data[key].author;
+
+      // Set attributes for the link, then add the title inside of it
+      resourceLink.setAttribute("href", data[key].link);
+      resourceLink.setAttribute("class", "book-link");
+      resourceLink.innerHTML = data[key].title + " &raquo;";
 
       /*
        * Arrange elements for an individual resource, then place it on
        * the page
        */
+      resourceContainer.appendChild( resourceLink );
       resourceContainer.appendChild( resourceTitle );
       resourceContainer.appendChild( resourceImage );
       resourceContainer.appendChild( resourceAuthor );
