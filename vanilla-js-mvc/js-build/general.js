8ab41af316754e487d63a70a48e530f769cf810c
Kai Gittens <kai.gittens@gmail.com>
2015-12-19 06:40:34 -0500

    require jquery, add comments to general.js

--- vanilla-js-mvc/js-build/general.js
+++ vanilla-js-mvc/js-build/general.js
@@ -3,3 +3,8 @@
 /* ============                                                      */
 /* A generic file  for scripts that don't have any real role in the  *//* MVC code                                                          */
 /* ================================================================= */
+var $ = require( "jquery" );
+
+$( window ).on( "scroll touchmove", function () {
+  $( "#header" ).toggleClass( "scale-logo", $( document ).scrollTop() > 0 );
+});
