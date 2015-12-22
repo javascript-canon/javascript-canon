a90babead294c18cad6246901b9fceb936c6a3c7
Kai Gittens <kai.gittens@gmail.com>
2015-12-22 07:22:07 -0500

    add hasOwnProperty() check..remove unneeded variables

--- vanilla-js-mvc/js-build/firstPageView.js
+++ vanilla-js-mvc/js-build/firstPageView.js
@@ -29,76 +29,71 @@ SingleResourceView.render = function( model ) {
 
   return resourcesData.on( "value", function( snapshot ) {
 
-    var
-        data, // will be a reference to the resource data on Firebase
-        componentArray = [];
-
     /*
      * Grab the Firebase data with a snapshot and store it in the data
      * variable. Read more about this at:
      * http://bit.ly/firebase-snapshot
      */
-    data = snapshot.val();
+    var data = snapshot.val();
 
     // Loop through the Firebase data to build elements
     for ( var key in data ) {
 
-      var pageTarget = document.getElementById( "targetEl" ),
-          resourceContainer = document.createElement( "article" ),
-          resourceTitle = document.createElement( "h2" ),
-          resourceImage = document.createElement( "img" ),
-          resourceAuthor = document.createElement( "h3" ),
-          resourceLink = document.createElement( "a" );
+      // Perform standard hasOwnProperty() check
+      if ( data.hasOwnProperty( key ) ) {
 
-      /*
-       * Setting attributes one-by-one instead of using something like 
-       * $.attr(). You can say that this code isn't cool, but it's 
-       * certainly faster.
-       *
-       * Check this at: http://bit.ly/set-attribute-test
-       */
+        var pageTarget = document.getElementById( "targetEl" ),
+            resourceContainer = document.createElement( "article" ),
+            resourceTitle = document.createElement( "h2" ),
+            resourceImage = document.createElement( "img" ),
+            resourceAuthor = document.createElement( "h3" ),
+            resourceLink = document.createElement( "a" );
+
+        /*
+         * Setting attributes one-by-one instead of using something
+         * like $.attr(). You can say that this code isn't cool, but
+         * it's certainly faster.
+         *
+         * Check this at: http://bit.ly/set-attribute-test
+         */
      
-      // Set attributes for the containing element
-      resourceContainer.setAttribute("class", "col-md-4 single-resource");
-      resourceContainer.setAttribute("data-resource-type", data[key].type);
+        // Set attributes for the containing element
+        resourceContainer.setAttribute( "class", "col-md-4 single-resource" );
+        resourceContainer.setAttribute( "data-resource-type", data[key].type );
 
-      // Add the resource title to the inside of the <h2>
-      //resourceTitle.innerHTML = data[key].title;
+        // Add the resource title to the inside of the <h2>
+        resourceTitle.setAttribute( "class", "gl-subheader" );
 
-      resourceTitle.setAttribute("class", "gl-subheader");
+        // Set attributes for the resource image
+        resourceImage.setAttribute( "src", "/img/book-images/" + data[key].image_large );
+        resourceImage.setAttribute( "class", "book-image" );
 
-      // Set attributes for the resource image
-      resourceImage.setAttribute( "src", "/img/book-images/" + data[key].image_large );
-      resourceImage.setAttribute( "class", "book-image" );
+        // Add the resource author to the inside of the <h3>
+        resourceAuthor.innerHTML =  "by " + data[key].author;
 
-      // Add the resource author to the inside of the <h3>
-      resourceAuthor.innerHTML =  "by " + data[key].author;
+        // Set attributes for the link, then add the title inside of it
+        resourceLink.setAttribute( "href", data[key].link );
+        resourceLink.setAttribute( "class", "book-link" );
+        resourceLink.innerHTML = data[key].title + " &raquo;";
 
-      // Set attributes for the link, then add the title inside of it
-      resourceLink.setAttribute("href", data[key].link);
-      resourceLink.setAttribute("class", "book-link");
-      resourceLink.innerHTML = data[key].title + " &raquo;";
+        /*
+         * Arrange elements for an individual resource, then place the
+         * resource on the page
+         */
 
-      /*
-       * Arrange elements for an individual resource, then place it on
-       * the page
-       */
+        resourceTitle.appendChild( resourceLink );
+        resourceContainer.appendChild( resourceTitle );
+        resourceContainer.appendChild( resourceImage );
+        resourceContainer.appendChild( resourceAuthor );
+        pageTarget.appendChild( resourceContainer );
 
-      resourceTitle.appendChild( resourceLink );
-      resourceContainer.appendChild( resourceTitle );
-      resourceContainer.appendChild( resourceImage );
-      resourceContainer.appendChild( resourceAuthor );
-      pageTarget.appendChild( resourceContainer );
-      componentArray.push( resourceContainer );
+      } //end hasOwnProperty() check
 
-    }
-    
-    // Not needed now, but keep it for future reference
-    // console.log( componentArray );
+    } // end for...in loop
 
-  });
+  }); // end "resourcesData.on()"
    
-}
+} // end "render()"
 
 
 // Export the page data so it's available to the page controller
