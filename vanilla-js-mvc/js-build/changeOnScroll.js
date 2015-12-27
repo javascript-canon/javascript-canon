5b211a30d7cc2ba7f4e861e49932874a715fcc08
Kai Gittens <kai.gittens@gmail.com>
2015-12-27 13:51:05 -0500

    add comments

--- vanilla-js-mvc/js-build/changeOnScroll.js
+++ vanilla-js-mvc/js-build/changeOnScroll.js
@@ -1,7 +1,7 @@
 /* ================================================================= */
 /* | changeOnScroll.js                                               */
 /* ===================                                               */
-/* A generic file  for scripts that don"t have any real role in      */
+/* A generic file  for scripts that don't have any real role in      */
 /* the MVC code code                                                 */
 /* ================================================================= */
 
@@ -9,10 +9,20 @@
 "use strict";
 
 // "require" jQuery core
-var $ = require( "jquery" );
+var
 
-// "require" jQuery core
-var waypoints = require( "waypoints" );
+    // "require" jquery
+    $ = require( "jquery" ),
+
+    /*
+     * "require" waypoints
+     *
+     * IMPORTANT: waypoints currently is NOT set up for CommonJS and
+     * cannot be simple come in as a Node require() with out
+     * the help of browserify-shim. Refer to its setup in package.json
+     * & read more about all this at: http://bit.ly/waypoints-shimming.
+     */
+    waypoints = require( "waypoints" );
 
 var waypoint = new Waypoint({
   element: document.getElementById( "h1-text" ),
