b19622557a960a2ee11ebe5856b4d12cf14942e4
Kai Gittens <kai.gittens@gmail.com>
2015-12-27 12:00:33 -0500

    update comments

--- vanilla-js-mvc/js-build/model.js
+++ vanilla-js-mvc/js-build/model.js
@@ -1,12 +1,19 @@
 /* ================================================================= */
 /* | MODEL DATA                                                      */
 /* ================================================================= */
+/*                                                                   */
+/*   - create a constructor function called "getJSONAPI()"           */
+/*   - have getJSONAPI() XHR in the Heroku JSON API                  */
+/*   - create getJSONAPI() instance called "ResourceModel" that      *//*     represents the model data                                     */
+/*   - export out ResourceModel so views and controllers can see it  */
+/* ================================================================= */
 
 // A very simple MVC implementation-read more at: http://bit.ly/1zxWh0m
 
 // use strict mode
 "use strict";
 
+// "require" jQuery core
 var $ = require( "jquery" );
 
 function getJSONAPI( url, data ){
