9e522291e01c04c8ce606d62c078f8b846a89f96
Kai Gittens <kai.gittens@gmail.com>
2015-12-27 21:12:02 -0500

    update vanilla-js build/ folder

--- vanilla-js-mvc/build/js/scripts.js
+++ vanilla-js-mvc/build/js/scripts.js
@@ -218,31 +218,40 @@ exports.SingleNavView = SingleNavView;
 /* ============                                                      */
 /* A generic file  for scripts that don't have any real role in      */
 /* the MVC code code                                                 */
+/*                                                                   */
+/* http://bit.ly/cool-sticky-code                                    */
 /* ================================================================= */
 
 // use strict mode
 "use strict";
 
-var $ = require("jquery");
+var $ = require( "jquery" );
 
 function init() {
-  console.log($("nav").height());
-  console.log($("nav").offset().top);
-  window.addEventListener('scroll', function( e ) {
 
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 150,
-      header = document.querySelector( "#header" ),
-      kai = $("nav").offset().top;
+  window.addEventListener( 'scroll', function( e ) {
+
+    var
+
+        distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      
+        shrinkOn = 150,
+
+        header = document.querySelector( "#header" ),
+
+        navTopOffset = $( "nav" ).offset().top;
   
     if ( distanceY > shrinkOn ) {
-      header.setAttribute( "style", "position:fixed" );
-      $(".nav-class").css("margin-top", kai );
-      $("#logo").attr("style", "display:none");
+
+      header.setAttribute("class", "header-class" );
+
+
+      $( ".nav-class" ).css( "margin-top", navTopOffset );
+      $( "#logo" ).attr( "style", "display:none" );
     } else {
-      header.setAttribute( "style", "position:static" );
-      $(".nav-class").css("margin-top", "0" );
-      $("#logo").attr("style", "");
+      header.removeAttribute( "class"  );
+      $( ".nav-class" ).css( "margin-top", "0" );
+      $( "#logo" ).attr( "style", "" );
     }
 
   });
