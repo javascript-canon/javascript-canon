4cdb228a3e6b0f569fb37f99b6092bf117ea470d
Kai Gittens <kai.gittens@gmail.com>
2015-12-27 22:17:20 -0500

    commit working h1 animation code

--- vanilla-js-mvc/js-build/changeOnScroll.js
+++ vanilla-js-mvc/js-build/changeOnScroll.js
@@ -24,17 +24,21 @@ function init() {
 
         header = document.querySelector( "#header" ),
 
+        h1 = document.querySelector( "#h1-text" ),
+
         navTopOffset = $( "nav" ).offset().top;
   
     if ( distanceY > shrinkOn ) {
 
       header.setAttribute( "class", "header-class" );
 
-
+     $(h1).addClass( "h1-animation" );
       $( ".nav-class" ).css( "margin-top", navTopOffset );
       $( "#logo" ).attr( "style", "display:none" );
     } else {
       header.removeAttribute( "class"  );
+
+      $(h1).removeClass( "h1-animation" );
       $( ".nav-class" ).css( "margin-top", "0" );
       $( "#logo" ).attr( "style", "" );
     }
