d1fdcc804ed44928900c18a84bfa9478d8c0fb61
Kai Gittens <kai.gittens@gmail.com>
2015-12-27 20:10:38 -0500

    update whitespace & comments

--- vanilla-js-mvc/js-build/changeOnScroll.js
+++ vanilla-js-mvc/js-build/changeOnScroll.js
@@ -3,31 +3,38 @@
 /* ============                                                      */
 /* A generic file  for scripts that don't have any real role in      */
 /* the MVC code code                                                 */
+/*                                                                   */
+/* http://bit.ly/cool-sticky-code                                    */
 /* ================================================================= */
 
 // use strict mode
 "use strict";
 
-var $ = require("jquery");
+var $ = require( "jquery" );
 
 function init() {
   console.log($("nav").height());
   console.log($("nav").offset().top);
   window.addEventListener('scroll', function( e ) {
 
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 150,
-      header = document.querySelector( "#header" ),
-      kai = $("nav").offset().top;
+    var
+
+        distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      
+        shrinkOn = 150,
+
+        header = document.querySelector( "#header" ),
+
+        kai = $("nav").offset().top;
   
     if ( distanceY > shrinkOn ) {
       header.setAttribute( "style", "position:fixed" );
-      $(".nav-class").css("margin-top", kai );
-      $("#logo").attr("style", "display:none");
+      $( ".nav-class" ).css( "margin-top", kai );
+      $( "#logo" ).attr( "style", "display:none" );
     } else {
       header.setAttribute( "style", "position:static" );
-      $(".nav-class").css("margin-top", "0" );
-      $("#logo").attr("style", "");
+      $( ".nav-class" ).css( "margin-top", "0" );
+      $( "#logo" ).attr( "style", "" );
     }
 
   });
