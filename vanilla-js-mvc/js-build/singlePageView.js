359ba255eed09874d95fe222e99d81b518fdff8e
Kai Gittens <kai.gittens@gmail.com>
2016-09-14 07:44:49 -0400

    start view code for a single page view

---
+++ vanilla-js-mvc/js-build/singlePageView.js
@@ -0,0 +1,38 @@
+/* ================================================================= */
+/* | VIEW FOR OPENING A SINGLE RESOURCE PAGE                         */
+/* ================================================================= */
+
+// A very simple MVC implementation-read more at: http://bit.ly/1zxWh0m
+
+// use strict mode
+"use strict";
+
+var
+
+    // "require" jQuery core
+    $ = require( "jquery" ),
+
+    // "require" the model data module so the view can access it
+    Resources = require( "./model" ),
+
+    // reference to the data model in the "Resources" module
+    resourcesData = Resources.ResourceModel,
+
+    // create view object for a single page view...export it out later
+    SinglePageResourceView = {};
+
+/*
+ * "render()" method renders info for single resource component.
+ * The "model" parameter will represent whatever variable is storing
+ * the model data
+ */
+SinglePageResourceView.render = function( model ) {
+
+} // end "render()"
+
+
+// Export the page data so it's available to the page controller
+exports.resourcesData = resourcesData;
+
+// Export the page view so it's available to the page controller
+exports.SinglePageResourceView = SinglePageResourceView;
