840266e1cd13fed5fc3dd40d4cb1d2595944c28e
Kai Gittens <kai.gittens@gmail.com>
2016-10-08 16:27:29 -0400

    remove whitespace

--- vanilla-js-mvc/js-build/firstPageView.js
+++ vanilla-js-mvc/js-build/firstPageView.js
@@ -10,10 +10,10 @@
 var
 
     // "require" jQuery core
-    $ = require( "jquery" ),
+    $ = require("jquery"),
 
     // "require" the model data module so the view can access it
-    Resources = require( "./model" ),
+    Resources = require("./model"),
 
     // reference to the data model in the "Resources" module
     resourcesData = Resources.ResourceModel,
@@ -26,25 +26,25 @@ var
  * The "model" parameter will represent whatever variable is storing
  * the model data
  */
-SingleResourceView.render = function( model ) {
+SingleResourceView.render = function(model) {
 
     // Grab the Heroku-powered model data
     var data = resourcesData;
 
     // Loop through the data to build elements
-    for ( var key in data ) {
+    for (var key in data) {
 
       // Perform standard hasOwnProperty() check
-      if ( data.hasOwnProperty( key ) ) {
+      if (data.hasOwnProperty(key)) {
 
-        var pageTarget = document.getElementById( "targetEl" ),
-            resourceContainer = document.createElement( "article" ),
-            resourceTitle = document.createElement( "h2" ),
-            resourceImage = document.createElement( "img" ),
-            resourceAuthor = document.createElement( "h3" ),
-            resourceLink = document.createElement( "a" ),
-            buyBookButton = document.createElement( "a" ),
-            modalButton = document.createElement( "a" );
+        var pageTarget = document.getElementById("targetEl"),
+            resourceContainer = document.createElement("article"),
+            resourceTitle = document.createElement("h2"),
+            resourceImage = document.createElement("img"),
+            resourceAuthor = document.createElement("h3"),
+            resourceLink = document.createElement("a"),
+            buyBookButton = document.createElement("a"),
+            modalButton = document.createElement("a");
 
         /*
          * Setting attributes one-by-one instead of using something
@@ -55,15 +55,15 @@ SingleResourceView.render = function( model ) {
          */
 
         // Set attributes for the containing element
-        resourceContainer.setAttribute( "id", key );
-        resourceContainer.setAttribute( "class", "col-md-4 single-resource" );
-        resourceContainer.setAttribute( "data-resource-type", data[key].type );
+        resourceContainer.setAttribute("id", key);
+        resourceContainer.setAttribute("class", "col-md-4 single-resource");
+        resourceContainer.setAttribute("data-resource-type", data[key].type);
 
         // Add the resource title to the inside of the <h2>
-        resourceTitle.setAttribute( "class", "gl-subheader" );
+        resourceTitle.setAttribute("class", "gl-subheader");
 
         // Set attributes for the resource image
-        resourceImage.setAttribute( "src", "/img/book-images/" + data[key].image_large );
+        resourceImage.setAttribute("src", "/img/book-images/" + data[key].image_large);
         resourceImage.setAttribute("class", "book-image");
 
         // Add the resource author to the inside of the <h3>
@@ -93,13 +93,13 @@ SingleResourceView.render = function( model ) {
          * resource on the page
          */
 
-        resourceTitle.appendChild( resourceLink );
-        resourceContainer.appendChild( resourceTitle );
-        resourceContainer.appendChild( resourceImage );
-        resourceContainer.appendChild( resourceAuthor );
-        resourceContainer.appendChild( buyBookButton );
-        resourceContainer.appendChild( modalButton );
-        pageTarget.appendChild( resourceContainer );
+        resourceTitle.appendChild(resourceLink);
+        resourceContainer.appendChild(resourceTitle);
+        resourceContainer.appendChild(resourceImage);
+        resourceContainer.appendChild(resourceAuthor);
+        resourceContainer.appendChild(buyBookButton);
+        resourceContainer.appendChild(modalButton);
+        pageTarget.appendChild(resourceContainer);
 
       } //end hasOwnProperty() check
 
