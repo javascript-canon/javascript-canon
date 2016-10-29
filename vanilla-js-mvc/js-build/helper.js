56017cd9da56c8ebb7e0fe2b776ff9bcc578168e
Kai Gittens <kai.gittens@gmail.com>
2016-10-29 05:32:36 -0400

    start helper.js

---
+++ vanilla-js-mvc/js-build/helper.js
@@ -0,0 +1,8 @@
+// "require" jQuery core
+var $ = require( "jquery" );
+
+// Click on "#btn-show-all" to make ALL the learning resources visible
+$("#btn-show-all").click(function(event) {
+  event.preventDefault();
+  $(".single-resource").css("display", "block");
+});
