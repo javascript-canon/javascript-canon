464175d379c8524df60334f7be50aebf5adb1a66
Kai Gittens <kai.gittens@gmail.com>
2016-10-30 17:46:54 -0400

    move close modal button code to helper.js

--- vanilla-js-mvc/js-build/helper.js
+++ vanilla-js-mvc/js-build/helper.js
@@ -6,3 +6,9 @@ $("#btn-show-all").click(function(event) {
   event.preventDefault();
   $(".single-resource").css("display", "block");
 });
+
+
+// When the user clicks on the modal's close button, close it
+$(".page-modal-element__button").click(function(){
+    SingleResourcePageController.closeModal();
+});
