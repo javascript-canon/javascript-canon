ff23466f0470ea0ab868ecf8b0ac6c1db80eb40e
Kai Gittens <kai.gittens@gmail.com>
2016-11-19 07:53:33 -0500

    rename id in jQuery click event

--- vanilla-js-mvc/js-build/helper.js
+++ vanilla-js-mvc/js-build/helper.js
@@ -2,7 +2,7 @@
 var $ = require( "jquery" );
 
 // Click on "#btn-show-all" to make ALL the learning resources visible
-$("#btn-show-all").click(function(event) {
+$("#btnShowAll").click(function(event) {
   event.preventDefault();
   $(".single-resource").css("display", "inline-block");
 });
