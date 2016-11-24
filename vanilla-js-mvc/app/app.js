87a98e2bc5e3ac501c66c3066039018dfb1fb187
Kai Gittens <kai.gittens@gmail.com>
2016-11-24 16:58:19 -0500

    add comments, update whitespace

--- vanilla-js-mvc/app/app.js
+++ vanilla-js-mvc/app/app.js
@@ -1,24 +1,66 @@
 var 
+    
+    // Require the express module
     express = require('express'),
+    
+    // Create a new express application called "app"
     app = express(),
+
+    // Point to the resource JSON data
     resourceData = require("./data/resources.json"),
+
+    // Require module for auto-refreshing the browser
     reload = require("reload"),
+
+    // Create a server variable for later use
     server;
 
+/*
+ * ==================
+ * START APP SETTINGS
+ * ==================
+ */
+
+// Set the development site port
 app.set("port", process.env.PORT || 3000 );
+
+// Set a pointer the resource data
 app.set("resources", resourceData);
+
+// Set the view template engins to be pug
 app.set("view engine", "pug");
+
+// Set where the views are
 app.set("views", "app/views");
 
 /*
- * "use" stuff in the app
+ * =================
+ * STOP APP SETTINGS
+ * =================
+ */
+
+
+
+/*
+ * =========================
+ * START MIDDLEWARE SETTINGS
+ * =========================
  */
 
  // grab asset files...images, .css, .js, etc.
 app.use(express.static('app/public'));
 
 // Routes
+
+// Index rout
 app.use(require('./routes/index'));
+
+/*
+ * ========================
+ * STOP MIDDLEWARE SETTINGS
+ * ========================
+ */
+
 
 // Run server
 server = app.listen(app.get("port"), function() {
