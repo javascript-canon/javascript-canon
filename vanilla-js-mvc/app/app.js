6b361ca2a4e7cf0e659fb2892268265aa60cb524
Kai Gittens <kai.gittens@gmail.com>
2016-11-24 10:13:52 -0500

    get app working by adding routes & views and configuring up app.js

--- vanilla-js-mvc/app/app.js
+++ vanilla-js-mvc/app/app.js
@@ -1,18 +1,20 @@
 var 
+    express = require('express'),
     app = express(),
-    resourceData = require('./js/resources.json'),
-    reload = require('reload'),
+    resourceData = require("./data/resources.json"),
+    reload = require("reload"),
     server;
 
-app.set('port', process.env.PORT || 3000 );
-app.set('resources', resourceData);
-app.set('view engine', 'ejs');
-app.set('views', 'app/views');
+app.set("port", process.env.PORT || 3000 );
+app.set("resources", resourceData);
+app.set("view engine", "ejs");
+app.set("views", "app/views");
 
+app.use(require('./routes/index'));
 
 // Run server
-server = app.listen(app.get('port'), function() {
-  console.log('Listening on port ' + app.get('port'));
+server = app.listen(app.get("port"), function() {
+  console.log("Listening on port " + app.get("port"));
 });
 
 // Reload the server when changes happen
