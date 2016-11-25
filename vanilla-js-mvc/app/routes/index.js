308210830fb498e88ef50cc2a970516e4375f5a4
Kai Gittens <kai.gittens@gmail.com>
2016-11-25 14:09:53 -0500

    require the resource data

--- vanilla-js-mvc/app/routes/index.js
+++ vanilla-js-mvc/app/routes/index.js
@@ -8,6 +8,7 @@ var router = require("express").Router();
 // "index.js" loads the homepage when browser calls the index route
 router.get("/", function(req, res) {
 
+	// Require the resource data
 	var data = req.app.get('resources');
 
 	// Render the index view with it's unique title
