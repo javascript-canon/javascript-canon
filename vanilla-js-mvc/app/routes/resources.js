c774650a8f939406a485ed391d265ba62627c25d
Kai Gittens <kai.gittens@gmail.com>
2016-11-26 07:57:11 -0500

    commit working resource route code before changing

--- vanilla-js-mvc/app/routes/resources.js
+++ vanilla-js-mvc/app/routes/resources.js
@@ -8,15 +8,18 @@ router.get('/resources/:resourceid', function(req, res) {
 	
 	// Require the resource data
 	var data = req.app.get('resources');
-	var resource = data.resources[req.params.resourceid];
-	
-	res.send(`
-    <h1>${resource.title}</h1>
-  `);
-console.log(resource);
+	var resourceTitles = [];
+
+	data.resources.forEach(function(item){
+		if(item.title == req.params.resourceid) {
+			resourceTitles.push(item);	
+		}
+	});
+
 	// Render the resource view with it's unique ids
   res.render("resources", {
-  	pageTitle: 'Resources'
+  	pageTitle: 'Resources',
+  	titles: resourceTitles
   });
 
 });
