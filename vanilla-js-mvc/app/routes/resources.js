6ef80d8b54c855ca9419c01369fa4f8ab5201381
Kai Gittens <kai.gittens@gmail.com>
2016-11-26 07:15:28 -0500

    commit working resource page code that uses ES6 template strings

--- vanilla-js-mvc/app/routes/resources.js
+++ vanilla-js-mvc/app/routes/resources.js
@@ -4,16 +4,17 @@
 // Create a router
 var router = require("express").Router();
 
-router.get('/resources/', function(req, res) {
+router.get('/resources/:resourceid', function(req, res) {
 	
 	// Require the resource data
 	var data = req.app.get('resources');
-
-	data.forEach(function(item) {
-    console.log("item", item.id);
-  });
-
-	// Render the resource view with it's unique title
+	var resource = data.resources[req.params.resourceid];
+	
+	res.send(`
+    <h1>${resource.title}</h1>
+  `);
+console.log(resource);
+	// Render the resource view with it's unique ids
   res.render("resources", {
   	pageTitle: 'Resources'
   });
