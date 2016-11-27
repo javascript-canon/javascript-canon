bf5704c06ea7ede13346f610ee7803f43a74585c
Kai Gittens <kai.gittens@gmail.com>
2016-11-26 22:20:50 -0500

    update whitespace

--- vanilla-js-mvc/app/routes/index.js
+++ vanilla-js-mvc/app/routes/index.js
@@ -8,9 +8,9 @@ var router = require("express").Router();
 // "index.js" loads the homepage when browser calls the index route
 router.get("/", function(req, res) {
 
-	// Render the index view with it's unique title
+  // Render the index view with it's unique title
   res.render("index", {
-  	pageTitle: "The JavaScript Canon - Vanilla JS"
+    pageTitle: "The JavaScript Canon - Vanilla JS"
   });
 
 });
