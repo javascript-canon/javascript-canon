25d377acd594b5a24b6770f7ca2792fbe39c8a7a
Kai Gittens <kai.gittens@gmail.com>
2016-11-27 07:51:06 -0500

    move the single resource route to index.js, update comments

--- vanilla-js-mvc/app/routes/index.js
+++ vanilla-js-mvc/app/routes/index.js
@@ -4,13 +4,39 @@
 // Create a router
 var router = require("express").Router();
 
-
-// "index.js" loads the homepage when browser calls the index route
+// Route for the home page
 router.get("/", function(req, res) {
 
   // Render the index view with it's unique title
   res.render("index", {
     pageTitle: "The JavaScript Canon - Vanilla JS"
+  });
+
+});
+
+// Route for a single learning resource
+router.get("/:singleresourceid", function(req, res) {
+
+  // Require the resource data
+  var
+
+      // Get the data being stored in the Node request object
+      requestData = req.app.get("resources"),
+
+      // Create a temporary array for the resource links
+      resourceLinks = [];
+
+  // Loop thru requestData, which is an array.
+  requestData.resources.forEach(function(item){
+    if(item.href == req.params.singleresourceid) {
+      resourceLinks.push(item);
+    }
+  });
+
+  // Render the resource view
+  res.render("resources", {
+    pageTitle: "Resources",
+    links: resourceLinks
   });
 
 });
--- vanilla-js-mvc/app/routes/resources.js
+++ vanilla-js-mvc/app/routes/resources.js
@@ -4,32 +4,5 @@
 // Create a router
 var router = require("express").Router();
 
-router.get("/:resourceid", function(req, res) {
-
-  // Require the resource data
-  var
-
-      // Get the data being stored in the Node request object
-      requestData = req.app.get("resources"),
-
-      // Create a temporary array for the resource links
-      resourceLinks = [];
-
-  // Loop thru requestData, which is an array.
-  requestData.resources.forEach(function(item){
-    if(item.href == req.params.resourceid) {
-      resourceLinks.push(item);
-    }
-  });
-
-  // Render the resource view
-  res.render("resources", {
-    pageTitle: "Resources",
-    links: resourceLinks
-  });
-
-});
-
-
 // Export the router out
 module.exports = router;
