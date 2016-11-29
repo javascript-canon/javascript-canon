757ca6af59d3a6a5b14fa7066d21eb25df5c689c
Kai Gittens <kai.gittens@gmail.com>
2016-11-29 07:00:41 -0500

    make sure req & res params are in the right spot, add comments & whitespace

--- backbone/index.js
+++ backbone/index.js
@@ -1,6 +1,35 @@
-var express = require("express");
-var app = module.exports = express();
+var
 
-app.get("/backbone", function(res, req){
-	res.render("backbone");
+		// Bring in express
+		express = require("express");
+
+		// Create "app" & export it out so the root "app.js" can access it
+    app = module.exports = express();
+
+
+
+/*
+ * ==================
+ * START APP SETTINGS
+ * ==================
+ */
+
+/* Point to views in the local folder
+ *
+ * TODO: get these views in subfolders
+ */
+app.set("views", __dirname);
+
+// Set the view engine to be EJS
+app.set("view engine", "ejs");
+
+/*
+ * =================
+ * STOP APP SETTINGS
+ * =================
+ */
+
+// Render the article template when going to the Backbone route
+app.get("/backbone", function(req, res) {
+	res.render("article");
 });
