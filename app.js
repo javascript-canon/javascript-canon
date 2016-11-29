613dc8dc8126bde5a6158ac4ec123e2079072962
Kai Gittens <kai.gittens@gmail.com>
2016-11-28 21:17:04 -0500

    treat the subfolders as middleware

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require("express"),
 		app = express();
 
+
+// Require sub-folders so we can see them from the index route
+var backbone = require("./backbone");
+var ember = require("./ember");
+var react = require("./react");
+
+// Mount the sub-folders as middleware
+app.use(backbone );
+app.use(ember);
+app.use(react);
+
+
 app.listen(3000);
 console.log("listening on 3000...")
