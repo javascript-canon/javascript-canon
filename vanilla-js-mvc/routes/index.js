9579884af7306f3bcc9679e086a1b11a9e46f2a6
Kai Gittens <kai.gittens@gmail.com>
2016-12-02 18:34:35 -0500

    remove uneeded route

--- vanilla-js-mvc/routes/index.js
+++ vanilla-js-mvc/routes/index.js
@@ -11,33 +11,5 @@ router.get("/vanilla-js-mvc", function(req, res) {
   });
 });
 
-// Route for a single learning resource
-router.get("/:singleresourceid", function(req, res) {
-
-  // Require the resource data
-  var
-
-      // Get the data being stored in the Node request object
-      requestData = req.app.get("resources"),
-
-      // Create a temporary array for the resource links
-      resourceLinks = [];
-
-  // Loop thru requestData, which is an array.
-  requestData.forEach(function(item){
-    if(item.href == req.params.singleresourceid) {
-      resourceLinks.push(item);
-    }
-  });
-
-  // Render the resource view
-  res.render("resources", {
-    pageTitle: "Resources",
-    links: resourceLinks
-  });
-
-});
-
-
 // Export the router out
 module.exports = router;
