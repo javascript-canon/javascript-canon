06bab12c282affc935282e55c1adf92f9ebd0baf
Kai Gittens <kai.gittens@gmail.com>
2016-12-01 22:37:26 -0500

    update vanilla js MVC's core app.js file (see message) add a "use strict" statement. require underscore.js. update routes. create a buildTypeNavigation function that dynamically builds the main nav. update comments and whitespaces

--- vanilla-js-mvc/index.js
+++ vanilla-js-mvc/index.js
@@ -1,13 +1,77 @@
+// use strict mode
+"use strict";
+
 var
 
     // Bring in express
-    express = require("express");
+    express = require("express"),
+
+    // Bring in underscore
+    _ = require("underscore"),
 
     // Create "app" & export it out so the root "app.js" can access it
     app = module.exports = express(),
 
     // Point to the resource JSON data
     resourceData = require(__dirname + "/data/resources.json");
+
+
+/*
+ * =============================
+ * START LOCAL VARIABLE SETTINGS
+ * =============================
+ */
+
+// An example's site title
+app.locals.exampleSiteTitle = "Vanilla JC MVC";
+
+// An example's URL
+app.locals.exampleURL = "vanilla-js-mvc";
+
+
+// buildTypeNavigation(): builds the main nav that lists resource types
+function buildTypeNavigation() {
+
+  var
+
+      // Array that will contain a list of resource types
+      getTypes = [],
+
+      // Array that will contain a list of filtered resource types
+      linkType;
+
+  // Loop through the model data to build the nav
+  for (var data in resourceData) {
+
+    // Do a standard hasOwnProperty() check against the model
+    if(resourceData.hasOwnProperty(data)) {
+
+      // Get "types" in the JSON & add them to the "getTypes" array
+      getTypes.push(resourceData[data].type);
+
+    } // end hasOwnProperty() check
+
+  } // end for...in loop
+
+  /* The "getTypes" array contains duplicate items at this point.
+   * Remove them with underscore's "uniq" method, store the results in
+   * a new array and assign it to the "linkType" variable.
+   */
+   linkType = _.uniq(getTypes);
+
+   // Assign "linkType" to a local express variable called "types"
+   return app.locals.types = linkType;
+
+ }
+
+// Run the function
+buildTypeNavigation();
+
+/*
+ * ============================
+ * STOP LOCAL VARIABLE SETTINGS
+ * ============================
+ */
 
 
 
@@ -35,23 +99,13 @@ app.set("view engine", "ejs");
 
 
 /*
- * =============================
- * START LOCAL VARIABLE SETTINGS
- * =============================
+ * ================
+ * CONFIGURE ROUTES
+ * ================
  */
 
-// Example site title
-app.locals.exampleSiteTitle = "Vanilla JC MVC";
-
-// Example URL
-app.locals.exampleURL = "vanilla-js-mvc";
-
-/*
- * ============================
- * STOP LOCAL VARIABLE SETTINGS
- * ============================
- */
-
+// Type routes...books, classes, etc.
+app.use(require('./routes/type'));
 
 
 /*
@@ -61,7 +115,7 @@ app.locals.exampleURL = "vanilla-js-mvc";
  */
 
  // grab asset files...images, .css, .js, etc.
-app.use(express.static(__dirname + "/public/"));
+ app.use(express.static(__dirname + "/public/"));
 
 /*
  * ========================
@@ -94,12 +148,14 @@ app.get("/vanilla-js-mvc/:singleresourceid", function(req, res) {
     if(item.href == req.params.singleresourceid) {
       resourceLinks.push(item);
     }
+
   });
 
   // Render the resource view
   res.render("resources", {
-    pageTitle: "Resources",
-    links: resourceLinks
+    links: resourceLinks,
+    pageTitle: req.params.singleresourceid
+
   });
 
 });
