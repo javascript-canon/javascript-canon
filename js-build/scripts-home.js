834c30d86cf1f4a72d64610d3cfa19fe39049998
Kai Gittens <kai.gittens@gmail.com>
2016-12-04 09:57:27 -0500

    setup homepage's JavaScript build process (see message) Create a "js-build/" folder for the homepage's JS source and have the source build out to "public/js/". Create "build-js-home" that performs this build process, run it in "watch-js-home" and then run that "watch-js-home" in "server-js-home". Delay the execution of all "server" tasks that run nodemon for 2 seconds.

---
+++ js-build/scripts-home.js
@@ -0,0 +1,48 @@
+// use strict mode
+"use strict";
+
+var
+
+  // Example data
+  examples = {
+    "Vanilla JS MVC": "vanilla-js-mvc",
+    "React": "react",
+    "Backbone.js": "backbone"
+  },
+
+  // Create document fragment to batch load the data all at once
+  documentFragment = document.createDocumentFragment(),
+
+  // Define the area where the examples links will load into
+  ulTargetElement = document.getElementById("examples");
+
+
+
+// Loop through the example data and place it on the home page
+for(var singleExample in examples) {
+
+  // Perform standard hasOwnProperty() check
+  if (examples.hasOwnProperty(singleExample)) {
+
+    // Dynamically create an <li> and <a> tag
+    var exampleItem = document.createElement("li"),
+        exampleLink = document.createElement("a");
+
+    // Add the text in the object key to the inside of the <a> tag
+    exampleLink.innerHTML = singleExample;
+
+    // Set the <a> tag's href attribute
+    exampleLink.setAttribute("href", "/" + examples[singleExample] + "/");
+
+    // Place the <a> tag inside the <li> tag
+    exampleItem.appendChild(exampleLink);
+
+    // Place the <li> and all its content inside the document fragment
+    documentFragment.appendChild(exampleItem);
+
+    // Load the document fragment inside the <ul> on the homepage
+    ulTargetElement.appendChild(documentFragment);
+
+  } // end hasOwnProperty() check
+
+}  // end for...in loop
