2802b952abbf4674b3bf14af7a2f5b38ade5be04
Kai Gittens <kai.gittens@gmail.com>
2016-12-04 07:26:50 -0500

    create single var pattern and add a document fragment

--- public/js/scripts-home.js
+++ public/js/scripts-home.js
@@ -1,5 +1,11 @@
-var examples = {
-  "Vanilla JS MVC": "vanilla-js-mvc",
-  "React": "react",
-  "Backbone.js": "backbone"
-};
+var
+
+  // Example data
+  examples = {
+    "Vanilla JS MVC": "vanilla-js-mvc",
+    "React": "react",
+    "Backbone.js": "backbone"
+  },
+
+  // Create document fragment to batch load the data all at once
+  documentFragment = document.createDocumentFragment();
