b016a0465ec4676d26b057471de99ce05855ea7f
Kai Gittens <kai.gittens@gmail.com>
2016-12-04 07:56:02 -0500

    start code that dynamically places links to examples onto the homepage

--- public/js/scripts-home.js
+++ public/js/scripts-home.js
@@ -8,4 +8,27 @@ var
   },
 
   // Create document fragment to batch load the data all at once
-  documentFragment = document.createDocumentFragment();
+  documentFragment = document.createDocumentFragment(),
+  targetElement = document.getElementById("examples");
+
+  for(var singleExample in examples) {
+
+    // Perform standard hasOwnProperty() check
+    if (examples.hasOwnProperty(singleExample)) {
+
+      var exampleItem = document.createElement("li");
+          exampleLink = document.createElement("a");
+
+      exampleLink.innerHTML = singleExample;
+
+      exampleLink.setAttribute("href", "/" + examples[singleExample] + "/");
+
+      exampleItem.appendChild(exampleLink);
+
+      documentFragment.appendChild(exampleItem);
+
+      targetElement.appendChild(documentFragment);
+
+    } // end hasOwnProperty() check
+
+  }  // end for...in loop
