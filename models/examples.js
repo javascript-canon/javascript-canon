929243ea61431fed3175a099daed5e015bde3eca
Kai Gittens <kai.gittens@gmail.com>
2016-12-11 06:21:29 -0500

    model the "examples" data

---
+++ models/examples.js
@@ -0,0 +1,19 @@
+// use strict mode
+"use strict";
+
+var
+
+    // Make it easier to create REST APIs with Node & Express
+    restful = require('node-restful'),
+
+    // Make a Mongo-based/Mongoose-powered REST API
+    mongoose = restful.mongoose;
+
+// Model "examples" data via a Mongo-based/Mongoose-powered REST API
+var examplesSchema = new mongoose.Schema({
+  name: String,
+  link: String
+});
+
+// Export "examples" collection based on the above Schema
+module.exports = restful.model('examples', examplesSchema);
