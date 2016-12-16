39fe18fd7e2afc09998f44df26dc9270bfbcae3d
Kai Gittens <kai.gittens@gmail.com>
2016-12-16 08:51:41 -0500

    update comments

--- js-build/scripts-home.js
+++ js-build/scripts-home.js
@@ -1,69 +1,70 @@
-// use strict mode
-"use strict";
+  // use strict mode
+  "use strict";
 
-var
+  var
 
-    // "require" jQuery core
-    $ = require("jquery"),
+      // "require" jQuery core
+      $ = require("jquery"),
 
-    RSVP = require("RSVP"),
+      // Require RSVP to polyfill Promise support...for IE mostly
+      RSVP = require("RSVP"),
 
-    Promise = window.Promise ||  RSVP.Promise,
+      /* Use Promises if there's native browser support for them...use
+       * RSVP if there isn't
+       */
+      Promise = window.Promise ||  RSVP.Promise,
 
-    // Create document fragment to batch load the data all at once
-    documentFragment = document.createDocumentFragment(),
+      // Create a document fragment to batch load the data all at once
+      documentFragment = document.createDocumentFragment(),
 
-    // Point to the <ul> target where the examples links will load into
-    ulTargetElement = document.getElementById("examples-target");
+      // Point to the <ul> target where the examples links will load into
+      ulTargetElement = document.getElementById("examples-target");
 
-/* An IIFE that checks if <ul id="examples-target" /> is on the page.
- * If it is run the buildExampleList()...if not, just do a "return
- * false". This should only run on the home page.
- */
-(function(){
-  if(ulTargetElement) {
-    return makeRequest();
-  } else {
-    return false;
-  }
-})();
+  /* An IIFE that checks if <ul id="examples-target" /> is on the page.
+   * If it is run the buildExampleList()...if not, just do a "return
+   * false". This should only run on the home page.
+   */
+  (function(){
+    if(ulTargetElement) {
+      return makeRequest();
+    } else {
+      return false;
+    }
+  })();
 
 
 
-/* Use Vanilla JS to AJAX in data after a Promise resolves. A simple
- * implementation I found on Stack Overflow at: http://bit.ly/2hvEK5U.
- */
-function makeRequest() {
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", '/api/resources');
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onload = function () {
-      if (this.status >= 200 && this.status < 300) {
-        resolve(xhr.response);
-      } else {
+  /* Use Vanilla JS to AJAX in data after a Promise resolves. A simple
+   * implementation I found on Stack Overflow at: http://bit.ly/2hvEK5U.
+   */
+  function makeRequest() {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", '/api/resources');
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.onload = function () {
+        if (this.status >= 200 && this.status < 300) {
+          resolve(xhr.response);
+        } else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          });
+        }
+      };
+      xhr.onerror = function () {
         reject({
           status: this.status,
           statusText: xhr.statusText
         });
-      }
-    };
-    xhr.onerror = function () {
-      reject({
-        status: this.status,
-        statusText: xhr.statusText
-      });
-    };
-    xhr.send();
+      };
+      xhr.send();
+    });
+  }
+
+  makeRequest()
+  .then(function (datums) {
+      console.log(datums);
+  }).catch(function(error) {
+    console.error(error);
   });
-}
-
-// Example:
-
-makeRequest()
-.then(function (datums) {
-    console.log(datums);
-}).catch(function(error) {
-  console.error(error);
-});
-
